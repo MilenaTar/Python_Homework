4b24e879ab15743b7501ee75bc224df1af26bb1e
MilenTar <tarverdyanmilena@yandex.ru>
2021-01-25 14:42:18 +0400

    Logging

--- Homework_OOP7.py
+++ Homework_OOP7.py
@@ -1,18 +1,18 @@
 import random
 import logging
 
-logging.basicConfig(filename= "Log_Example.txt", filemode = "w",level=logging.DEBUG)
+logging.basicConfig(filename= "Log_Example.txt", filemode = "w",format = ' %(message)s time :%(asctime)s',level=logging.DEBUG)
 for i in range(10):
 	num = random.randint(0,50)
 	if num in range(0,10):
-		print(num)
-		logging.debug(F'number is in 0-9:{num}') 
+		logging.debug(F'Debug message: number is in 0-9:{num}') 
 	if num in range(10,20):
 		print(num)
-		logging.info(F"Number is in 10-19: {num}")
+		logging.info(F"Infor message: number is in 10-19: {num}")
 	if num in range(20,30):
-		logging.warning(F"Number is in 20-29: {num}")
+		logging.warning(F"Warning message: number is in 20-29: {num}")
 	elif num in range(30,40):
-		logging.error(F"Number is in 30-39: {num}")
+		logging.error(F"Error message: number is in 30-39: {num}")
 	elif num in range(40,51):
-		logging.critical(F"Number is in 40-49:{num}")
+		logging.critical(F"Critical message: number is in 40-49:{num}")
+print("THE END")
